59be8f3035d34b59b72448231f34b3a995b7b6c9
dell <kawtar.kinadk17@gmail.com>
2024-05-15 16:16:04 +0100

    DatabaseConnection

--- src/java/util/ConnectionFactory.java
+++ src/java/util/ConnectionFactory.java
@@ -4,19 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://localhost:3306/cosmprodb"; // Change the URL as per your MySQL database configuration
-    private static final String USERNAME = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = ""; // Replace with your MySQL password
 
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
+public class ConnectionFactory {
+    
+  public static Connection getConnection() {
+		Connection con = null;
+		String url = "jdbc:mysql://127.0.0.1:3306/cosmprodb";
+		String username = "root";
+		String password = "";
+		try {
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			con = DriverManager.getConnection(url, username, password);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
